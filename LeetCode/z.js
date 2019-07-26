7d1fd7b9877e0ed6287de113aa32cc372670440d
1185491736@qq.com <1185491736@qq.com>
2019-07-26 18:51:48 +0800

    LeetCode

--- LeetCode/z.js
+++ LeetCode/z.js
@@ -1,16 +1,7 @@
-let comb = arr => {
-    let tmp = [];
-    for(let i = 0; i<arr[0].length; i++) {
-        for(let j = 0; j<arr[1].length; j++) {
-            tmp.push(`${arr[0][i]}${arr[1][j]}`)
-        }
-    }
-    arr.splice(0,2,tmp);
-    if(arr.length > 1) {
-        comb(arr)
-    }else {
-        return tmp
-    }
-}
-const arr = ['abc','def','ghi'];
-console.log(comb(['abc','def','ghi']));
+const a = [2,2,2,2];
+const b = 2;
+console.log(
+    a.every(elm => {
+        return !(elm%b !== 0)
+    })
+)
