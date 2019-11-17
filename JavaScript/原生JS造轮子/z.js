c2a69408d8e83c126621fc46d4c65b24d0d96503
1185491736@qq.com <1185491736@qq.com>
2019-11-17 14:14:35 +0800

    寄生继承

--- JavaScript/原生JS造轮子/z.js
+++ JavaScript/原生JS造轮子/z.js
@@ -8,3 +8,28 @@ const b = a.replace(match2, '')
 console.log(
   b.split(' ')[1]
 )
+
+console.log(
+  [] == ![],
+  Boolean('')
+)
+
+function Person(name) {
+  this.name = name
+}
+const aihao = new Person('aihao')
+console.log(
+  Person.prototype === aihao.__proto__
+)
+
+function Student(job) {
+  this.job = job
+}
+
+Student.prototype = Object.create(Person.prototype)
+
+const aihao2 = new Student('frontend')
+console.log(
+  aihao,
+  Student.prototype.__proto__ === Person.prototype
+)
