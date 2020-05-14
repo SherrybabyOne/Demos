c6776f6e86038ab37e911dc993b89fccb7bc20cd
aihao <1185491736@qq.com>
2020-05-14 11:15:41 +0800

    feat: instanceof

---
+++ JavaScript/instanceof.js
@@ -0,0 +1,13 @@
+const myInstanceof = (left, right) => {
+  // 基本数据类型都返回false
+  if (typeof left !== 'object' || left === null) return false;
+  let proto = Object.getPrototypeOf(left);
+  while (true) {
+    if (proto === null) return false;
+    if (proto === right.prototype) return true;
+    proto = Object.getPrototypeOf(proto);
+  }
+}
+
+console.log(myInstanceof("111", String)); //false
+console.log(myInstanceof(new String("111"), String));//true
---
+++ z.js
@@ -0,0 +1,6 @@
+console.log(
+  typeof new Object('1111'),
+  0.1 + 0.2,
+  123 instanceof Object,
+  Object.getPrototypeOf(123),
+);
