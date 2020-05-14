8b621112f66017e62e933c693b76b186bfaa3116
aihao <1185491736@qq.com>
2020-05-14 18:29:10 +0800

    feat: 数组扁平

---
+++ JavaScript/扁平操作.js
@@ -0,0 +1,34 @@
+// 将数组进行扁平操作
+const arr = [1, [2, [3, [4, 5]]], 6];
+
+const res1 = arr.flat(Infinity);
+
+const res2 = JSON.stringify(arr).replace(/\[|\]/g, '').split(',');
+
+const res3 = JSON.parse('[' + JSON.stringify(arr).replace(/\[|\]/g, '') + ']');
+
+const flatten = arr => {
+  return arr.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? flatten(cur) : cur);
+  }, [])
+}
+
+let res5 = [];
+const fn = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      fn(arr[i]);
+    } else {
+      res5.push(arr[i]);
+    }
+  }
+}
+fn(arr);
+
+console.log(
+  res1,
+  res2,
+  res3,
+  flatten(arr),
+  res5,
+);
