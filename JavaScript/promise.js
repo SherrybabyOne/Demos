e297e3ab10843c3521afa99bd789c8e095845126
aihao <1185491736@qq.com>
2020-05-18 22:25:07 +0800

    feat: Promise初步实现

---
+++ JavaScript/promise.js
@@ -0,0 +1,13 @@
+// 模拟实现Promise
+// Promise利用三大手段解决回调地狱：
+// 1. 回调函数延迟绑定
+// 2. 返回值穿透
+// 3. 错误冒泡
+
+// 定义三种状态
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+function myPromise() {
+
+}
