9e97c80213f4f54b8b6bf802af5690a40b812ad1
aihao <1185491736@qq.com>
2020-08-11 22:50:28 +0800

    feat: promiseAll

---
+++ JavaScript/promiseAll.js
@@ -0,0 +1,26 @@
+Promise.myAll = function(promiseArr) {
+  return new Promise((resolve, reject) => {
+    const ans = [];
+    let index = 0;
+    for (let i = 0; i < promiseArr.length; i++) {
+      promiseArr[i].then(res => {
+        ans[i] = res;
+        index++;
+        if (index === promiseArr.length) {
+          resolve(ans);
+        }
+      })
+    }
+  })
+}
+
+var p1 = Promise.resolve(3);
+var p2 = Promise.resolve(1337);
+var p3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+}); 
+
+Promise.myAll([p1, p2, p3]).then(values => { 
+  console.log(values); // [3, 1337, "foo"] 
+});
+// expected output: Array [3, 42, "foo"]
