7bb542a121b09163897d5ed15d560315a5210f4c
aihao <1185491736@qq.com>
2020-08-12 18:19:28 +0800

    feat: promise

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -5,60 +5,150 @@
 // 3. 错误冒泡
 
 // 定义三种状态
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
-function MyPromise(exector) {
-  const that = this;
-  that.status = PENDING;
-  that.value = null;
-  that.error = null;
-  // 成功态回调队列
-  that.onFulfilledCallbacks = [];
-  // 失败态回调队列
-  that.onRejectedCallbacks = [];
+const PENDING = 'PENDING';      // 进行中
+const FULFILLED = 'FULFILLED';  // 已成功
+const REJECTED = 'REJECTED';    // 已失败
 
-  const resolve = value => {
-    if (value instanceof MyPromise) {
-      return value.then(resolve, reject);
+class Promise {
+  constructor(exector) {
+    // 初始化状态
+    this.status = PENDING;
+    // 将成功、失败结果放在this上，便于then、catch访问
+    this.value = undefined;
+    this.reason = undefined;
+    // 成功态回调函数队列
+    this.onFulfilledCallbacks = [];
+    // 失败态回调函数队列
+    this.onRejectedCallbacks = [];
+
+    const resolve = value => {
+      // 只有进行中状态才能更改状态
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        // 成功态函数依次执行
+        this.onFulfilledCallbacks.forEach(fn => fn(this.value));
+      }
     }
-    setTimeout(() => {
-      if (that.status === PENDING) {
-        that.value = value;
-        that.staus = FULFILLED;
-        that.onFulfilledCallbacks.map(cb => {
-          that.value = cb(that.value);
-        })
+    const reject = reason => {
+      // 只有进行中状态才能更改状态
+      if (this.status === PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        // 失败态函数依次执行
+        this.onRejectedCallbacks.forEach(fn => fn(this.reason))
       }
-    }, 0)
+    }
+    try {
+      // 立即执行executor
+      // 把内部的resolve和reject传入executor，用户可调用resolve和reject
+      exector(resolve, reject);
+    } catch(e) {
+      // executor执行出错，将错误内容reject抛出去
+      reject(e);
+    }
   }
-  const reject = error => {
-    setTimeout(() => {
-      if (that.status === PENDING) {
-        that.error = error;
-        that.status = REJECTED;
-        that.onRejectedCallbacks.map(cb => {
-          that.error = cb(that.error);
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function'? onRejected:
+      reason => { throw new Error(reason instanceof Error ? reason.message:reason) }
+    // 保存this
+    const self = this;
+    return new Promise((resolve, reject) => {
+      if (self.status === PENDING) {
+        self.onFulfilledCallbacks.push(() => {
+          // try捕获错误
+          try {
+            // 模拟微任务
+            setTimeout(() => {
+              const result = onFulfilled(self.value);
+              // 分两种情况：
+              // 1. 回调函数返回值是Promise，执行then操作
+              // 2. 如果不是Promise，调用新Promise的resolve函数
+              result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+            })
+          } catch(e) {
+            reject(e);
+          }
+        });
+        self.onRejectedCallbacks.push(() => {
+          // 以下同理
+          try {
+            setTimeout(() => {
+              const result = onRejected(self.reason);
+              // 不同点：此时是reject
+              result instanceof Promise ? result.then(resolve, reject) : reject(result);
+            })
+          } catch(e) {
+            reject(e);
+          }
         })
+      } else if (self.status === FULFILLED) {
+        try {
+          setTimeout(() => {
+            const result = onFulfilled(self.value);
+            result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+          });
+        } catch(e) {
+          reject(e);
+        }
+      } else if (self.status === REJECTED){
+        try {
+          setTimeout(() => {
+            const result = onRejected(self.reason);
+            result instanceof Promise ? result.then(resolve, reject) : reject(result);
+          })
+        } catch(e) {
+          reject(e);
+        }
       }
-    }, 0)
+    });
   }
-  exector(resolve, reject);
-}
-MyPromise.prototype.then = function(onFulfilled, onRejected) {
-  const that = this;
-  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
-  onRejected = typeof onRejected === 'function' ? onRejected : r => { throw r };
-  if (that.status === PENDING) {
-    that.onFulfilledCallbacks.push(onFulfilled);
-    that.onRejectedCallbacks.push(onRejected);
-  } else if (that.status === FULFILLED) {
-    onFulfilled(that.value);
-  } else {
-    onRejected(that.error);
+  catch(onRejected) {
+    return this.then(null, onRejected);
   }
-  // 返回this支持then方法可以被同一个promise调用多次
-  return that;
+  static resolve(value) {
+    if (value instanceof Promise) {
+      // 如果是Promise实例，直接返回
+      return value;
+    } else {
+      // 如果不是Promise实例，返回一个新的Promise对象，状态为FULFILLED
+      return new Promise((resolve, reject) => resolve(value));
+    }
+  }
+  static reject(reason) {
+    return new Promise((resolve, reject) => {
+      reject(reason);
+    })
+  }
+  static all(promiseArr) {
+    const len = promiseArr.length;
+    const values = new Array(len);
+    // 记录已经成功执行的promise个数
+    let count = 0;
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < len; i++) {
+        // Promise.resolve()处理，确保每一个都是promise实例
+        Promise.resolve(promiseArr[i]).then(
+          val => {
+            values[i] = val;
+            count++;
+            // 如果全部执行完，返回promise的状态就可以改变了
+            if (count === len) resolve(values);
+          },
+          err => reject(err),
+        );
+      }
+    })
+  }
+  static race(promiseArr) {
+    return new Promise((resolve, reject) => {
+      promiseArr.forEach(p => {
+        Promise.resolve(p).then(
+          val => resolve(val),
+          err => reject(err),
+        )
+      })
+    })
+  }
 }
-
-MyPromise.resolve(4).then().then((value) => console.log(value))
